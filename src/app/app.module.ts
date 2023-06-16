951988a5d0de97e805512332b5ce5a948df09ffc
JoaoVictorMedeiros <joaovictormedeiros1505@gmail.com>
2023-06-16 17:31:42 -0300

    fixing some peaces of breakcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import {HomeComponent} from './home/home.component';
+import {HomeComponent} from './components/home/home.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
-import { HeaderComponent } from './home/header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatListModule} from "@angular/material/list";
@@ -13,12 +13,18 @@ import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CarouselModule} from "ngx-bootstrap/carousel";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import { AppRoutingModule } from './app-routing.module';
+import { AutomobileComponent } from './components/automobile/automobile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    AutomobileComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +36,11 @@ import {CarouselModule} from "ngx-bootstrap/carousel";
     MatCardModule,
     MatButtonModule,
     BrowserAnimationsModule,
-    CarouselModule
+    CarouselModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatMenuModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
