2cd433ecdc646d44e042d25d4c34c1172974d740
JoaoVictorMedeiros <joaovictormedeiros1505@gmail.com>
2023-06-17 15:48:32 -0300

    making automobile component works

--- src/app/components/automobile/automobile.component.ts
+++ src/app/components/automobile/automobile.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SharedService} from "../../shared/shared.service";
 
 @Component({
   selector: 'app-automobile',
   templateUrl: './automobile.component.html',
   styleUrls: ['./automobile.component.css']
 })
-export class AutomobileComponent {
+export class AutomobileComponent implements OnInit {
+  auto: any = [];
+  constructor(private service: SharedService) {
+  }
 
+  ngOnInit(): void {
+    this.service.listAutomobileNotice().subscribe((data: any) => {
+      this.auto = data.results;
+    });
+  }
 }
