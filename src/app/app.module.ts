2accef2a464a3223dab41db2d08166ed5cd83898
JoaoVictorMedeiros <joaovictormedeiros1505@gmail.com>
2023-06-17 22:20:07 -0300

    removing import of a deleting component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,24 @@ import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {HomeComponent} from './components/home/home.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
-import { HeaderComponent } from './shared/header/header.component';
+import {HeaderComponent} from './shared/header/header.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatListModule} from "@angular/material/list";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CarouselModule} from "ngx-bootstrap/carousel";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatIconModule} from "@angular/material/icon";
 import {MatMenuModule} from "@angular/material/menu";
-import { AppRoutingModule } from './app-routing.module';
-import { AutomobileComponent } from './components/automobile/automobile.component';
-import { BusinessComponent } from './components/business/business.component';
-import { ArtsComponent } from './components/arts/arts.component';
-import { HealthComponent } from './components/health/health.component';
-import { TechnologyComponent } from './components/technology/technology.component';
-import { CrosswordsComponent } from './components/crosswords/crosswords.component';
-import { FoodComponent } from './components/food/food.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AutomobileComponent} from './components/automobile/automobile.component';
+import {BusinessComponent} from './components/business/business.component';
+import {ArtsComponent} from './components/arts/arts.component';
+import {HealthComponent} from './components/health/health.component';
+import {TechnologyComponent} from './components/technology/technology.component';
+import {FoodComponent} from './components/food/food.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +34,6 @@ import { FoodComponent } from './components/food/food.component';
     ArtsComponent,
     HealthComponent,
     TechnologyComponent,
-    CrosswordsComponent,
     FoodComponent
   ],
   imports: [
