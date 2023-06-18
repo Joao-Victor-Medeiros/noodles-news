4c3b61495b84ae46e5d3743785cd21bac13215cc
JoaoVictorMedeiros <joaovictormedeiros1505@gmail.com>
2023-06-17 22:07:57 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home/home.component";
 import {AutomobileComponent} from "./components/automobile/automobile.component";
+import {BusinessComponent} from "./components/business/business.component";
+import {ArtsComponent} from "./components/arts/arts.component";
+import {HealthComponent} from "./components/health/health.component";
+import {TechnologyComponent} from "./components/technology/technology.component";
+import {FoodComponent} from "./components/food/food.component";
 
 const appRoutes: Routes = [
   {path: 'home', component: HomeComponent},
   { path: 'automobiles', component: AutomobileComponent },
+  { path: 'business', component: BusinessComponent },
+  { path: 'arts', component: ArtsComponent },
+  { path: 'food', component: FoodComponent },
+  { path: 'health', component: HealthComponent },
+  { path: 'technologies', component: TechnologyComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
 ]
 
